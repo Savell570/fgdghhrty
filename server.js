a4fc66c9621f0f6f055fcdaa8a7487fd88abae06
Glitch (music-tuto) <none>
2020-07-27 04:42:17 +0000

    📚📻 Checkpoint ./server.js:19003137/329

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYP
 const { readdirSync } = require("fs");
 const { join } = require("path");
 const { TOKEN, PREFIX } = require("./config.json")
-
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 //CLIENT EVENTS
 client.on("ready", () => {
   console.log('Bot is ready to play songs! | Bot created by 0_0')
