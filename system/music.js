210c3d0c145125668e5b38d93648fa3aec26098c
Glitch (music-bot-tutorial) <none>
2020-11-17 05:40:57 +0000

    🌚⛈ Checkpoint ./config.json:19003137/47 ./commands/ping.js:19003137/2184 ./system/music.js:19003137/11198 ./commands/queue.js:19003137/1933 ./package.json:19003137/72

--- system/music.js
+++ system/music.js
@@ -1,22 +1,20 @@
-//so bored lmao
 const ytdlDiscord = require("ytdl-core-discord");
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 const { QUEUE_LIMIT, COLOR } = require("../config.json");
 
 module.exports = {
   async play(song, message) {
     const queue = message.client.queue.get(message.guild.id);
-let embed = new MessageEmbed()
-.setColor(COLOR);
+    let embed = new MessageEmbed().setColor(COLOR);
 
     if (!song) {
       queue.channel.leave();
       message.client.queue.delete(message.guild.id);
-      embed.setAuthor("Dispatcher queue ended | Opus Returned a Queue End")
-      embed.setDescription(`Ended without playing a song? DM 0_0#6666 for assistance!`)
-      return queue.textChannel
-        .send(embed)
-        .catch(console.error);
+      embed.setAuthor("Dispatcher queue ended | Opus Returned a Queue End");
+      embed.setDescription(
+        `Ended without playing a song? DM 0_0#6666 for assistance!`
+      );
+      return queue.textChannel.send(embed).catch(console.error);
     }
 
     try {
@@ -49,11 +47,15 @@ let embed = new MessageEmbed()
         }
       })
       .on("error", console.error);
-  
+
     dispatcher.setVolumeLogarithmic(queue.volume / 100); //VOLUME
-embed.setAuthor("💿 | Started Playing Your Song", message.client.user.displayAvatarURL())
-    .setDescription(`**[${song.title}](${song.url})**`)
-    
+    embed
+      .setAuthor(
+        "💿 | Started Playing Your Song",
+        message.client.user.displayAvatarURL()
+      )
+      .setDescription(`**[${song.title}](${song.url})**`);
+
     queue.textChannel
       .send(embed)
       .catch(err => message.channel.send("UNABLE TO PLAY SONG"));
