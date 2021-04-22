68403885314b5f9f9d25ddae5da13cc7ea708ea9
Glitch (music-bot-tutorial) <none>
2021-04-22 07:55:11 +0000

    🐜🎺 Checkpoint ./commands/shuffle.js:19003137/2651

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,19 +1,30 @@
- 
+const { MessageEmbed } = require("discord.js");
 
-const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("There is no queue.",message.channel).catch(console.error);
-try{
-    let songs = serverQueue.songs;
-    for (let i = songs.length - 1; i > 1; i--) {
-      let j = 1 + Math.floor(Math.random() * i);
-      [songs[i], songs[j]] = [songs[j], songs[i]];
+const { COLOR } = require("../config.json");
+
+module.exports = {
+  name: "shuffle",
+  description: "Shuffle your queue and have fun!",
+  aliases: ["sh"],
+  async execute(client, message, args) {
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue)
+      return message.channel.send("There are no songs that I could shuffle");
+    try {
+      let songs = serverQueue.songs;
+      for (let m = songs.length - 1; m > 1; m--) {
+        let x = 1 + Math.floor(Math.random() * m);
+        [songs[m], songs[x]] = [songs[x], songs[m]];
+      }
+      serverQueue.songs = songs;
+      await message.client.queue.set(message.guild.id, serverQueue);
+      message.react("🔀");
+    } catch (error) {
+      message.guild.member.voice.channel.leave();
+      message.client.queue.delete(message.guild.id);
+      return message.channel.send(
+        `:notes: The player has stopped and the queue has been cleared.: \`${error}\``
+      );
     }
-    serverQueue.songs = songs;
-    message.client.queue.set(message.guild.id, serverQueue);
-    message.react("✅")
-      } catch (error) {
-        message.guild.me.voice.channel.leave();
-        message.client.queue.delete(message.guild.id);
-        return sendError(`:notes: The player has stopped and the queue has been cleared.: \`${error}\``, message.channel);
-     }
-  },
+  }
+};
