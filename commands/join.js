e5b63fb3b779f2b470ce3f7c0df09ac4af444626
Glitch (music-bot-tutorial) <none>
2021-04-22 06:20:55 +0000

    🏬💆 Checkpoint ./commands/join.js:19003137/1542

---
+++ commands/join.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+const { COLOR } = require("../config.json");
+module.exports = {
+  name: "join",
+  description: "Join a specific voice channel you are in",
+  async execute(client, message, args) {
+   let channel = message.member.voice.channel;
+        if (!channel) return message.channel.send("I'm sorry but you need to be in a voice channel!", message.channel);
+
+        try {
+            const connection = await channel.join();
+        } catch (error) {
+            console.error(`I could not join the voice channel: ${error}`);
+        }
+
+        const embed = new MessageEmbed()
+            .setAuthor("Joined Voice Channel")
+            .setColor("#fffdd0")
+            .setTitle(`Success`)
+            .setDescription(`🎶 Joined The Voice Channel. ${channel}.`)
+            .setTimestamp();
+
+        return message.channel.send(embed);
+}
+}
